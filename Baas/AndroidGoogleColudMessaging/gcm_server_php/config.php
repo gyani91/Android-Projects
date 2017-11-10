943700760ed05dea29d1a19c1905ae305243dfa3
Sumeet Gyanchandani <sumeet.gyanchandani@gmail.com>
2017-11-10 17:15:32 +0100

    Initial Upload

---
+++ Baas/AndroidGoogleColudMessaging/gcm_server_php/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "mysql");
+define("DB_DATABASE", "gcm");
+
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyDcEgafDQdDhk5TGmcweFbyMt3cTezh3uc"); 
+// Place your Google API Key
+?>
